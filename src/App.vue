13235206a93b6279a0cd94cf07fff86885827909
fengtao <1172103585@qq.com>
2020-03-11 16:02:06 +0800

    el-table添加宽度,不然会无限宽度

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,13 @@
       <Left />
       <div class="content">
         <el-breadcrumb style="margin: 15px;" separator-class="el-icon-arrow-right">
-          <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-          <el-breadcrumb-item>活动管理</el-breadcrumb-item>
-          <el-breadcrumb-item>活动列表</el-breadcrumb-item>
-          <el-breadcrumb-item>活动详情</el-breadcrumb-item>
+          <el-breadcrumb-item :to="{path: '/adduser'}">用户管理</el-breadcrumb-item>
+          <el-breadcrumb-item :to="{path: '/'}">用户列表</el-breadcrumb-item>
+          <el-breadcrumb-item >用户添加</el-breadcrumb-item>
         </el-breadcrumb>
-        <router-view />
+        <div style="width: 1000px">
+          <router-view />
+        </div>
       </div>
     </div>
   </div>
@@ -32,13 +33,11 @@ export default {
 #app {
   display: flex;
   height: 100vh;
+  width: 100vw;
   flex-direction: column;
   .container {
     flex: 1;
     display: flex;
-    .content {
-      flex: 1;
-    }
   }
 }
 </style>
--- src/view/user/userlist.vue
+++ src/view/user/userlist.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-table :data="tableData" border style="width: 100%">
+  <el-table :data="tableData" border>
     <el-table-column prop="date" label="日期" width="180"></el-table-column>
     <el-table-column prop="name" label="姓名" width="180"></el-table-column>
     <el-table-column prop="address" label="地址"></el-table-column>
